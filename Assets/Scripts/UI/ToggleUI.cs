d3e952394aac97ae8f9ce515edaec5d97aa2c336
Casper von Krusenstierna <caspervonkrusenstierna@gmail.com>
2026-02-06 21:00:49 +0100

    Fixed bug with settings visibility toggle

--- Assets/Scripts/UI/ToggleUI.cs
+++ Assets/Scripts/UI/ToggleUI.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class ToggleUI : MonoBehaviour
 {
-    public GameObject settingsCanvas;
+    public CanvasGroup settingsCanvas;
 
     void Update()
     {
-
-        if (OVRInput.GetDown(OVRInput.Button.Three) || Keyboard.current.yKey.wasPressedThisFrame) // Y button
+        if (OVRInput.GetDown(OVRInput.Button.Three) ||
+            Keyboard.current.yKey.wasPressedThisFrame)
         {
-            settingsCanvas.SetActive(!settingsCanvas.activeSelf);
+            bool isOpen = settingsCanvas.alpha > 0;
+
+            settingsCanvas.alpha = isOpen ? 0 : 1;
+            settingsCanvas.interactable = !isOpen;
+            settingsCanvas.blocksRaycasts = !isOpen;
         }
     }
 }
