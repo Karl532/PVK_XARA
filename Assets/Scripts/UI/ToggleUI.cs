d630fbed638e2a6c703ecdf0f053dec7054f8d61
Casper von Krusenstierna <caspervonkrusenstierna@gmail.com>
2026-02-06 11:44:06 +0100

    Made ui follow camera

--- Assets/Scripts/UI/ToggleUI.cs
+++ Assets/Scripts/UI/ToggleUI.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
-
-public class SettingsOverlayToggle : MonoBehaviour
+using UnityEngine.InputSystem;
+public class ToggleUI : MonoBehaviour
 {
     public GameObject settingsCanvas;
 
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.Three)) // Y button
+
+        if (OVRInput.GetDown(OVRInput.Button.Three) || Keyboard.current.yKey.wasPressedThisFrame) // Y button
         {
             settingsCanvas.SetActive(!settingsCanvas.activeSelf);
         }
---
+++ Assets/Scripts/UI/UIFollowCamera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class UIFollowCamera : MonoBehaviour
+{
+    public Transform xrCamera;
+    public Vector3 offset = new Vector3(0, 0, 2); // 2 meters in front
+
+    void LateUpdate()
+    {
+        if (xrCamera == null) return;
+
+        transform.position = xrCamera.position + xrCamera.forward * offset.z + 
+                             xrCamera.up * offset.y + xrCamera.right * offset.x;
+
+        transform.LookAt(xrCamera);
+        transform.Rotate(0, 180f, 0);
+    }
+}
